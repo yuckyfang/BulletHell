00049b3f2177ddb869b0fc9395ee8bd4533f22c9
Student 4 <Student 4@10.8.33.120>
2018-04-23 16:21:13 -0400

    gg

--- BulletHell/src/application/Sbullet.java
+++ BulletHell/src/application/Sbullet.java
@@ -1,6 +1,11 @@
 package application;
 
-public class Sbullet implements Bullet
+public class Sbullet extends Bullet
 {
 	
+
+	
+	public Sbullet() {
+		super(0,0,1,5,0,false, "A", new int[]{5,5}); 
+	}
 }
