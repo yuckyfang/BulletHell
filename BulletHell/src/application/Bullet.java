af2071f1f5d3425de5de585cf79c81142d6452d5
Student 4 <Student 4@10.8.33.120>
2018-04-23 16:21:25 -0400

    aa

--- BulletHell/src/application/Bullet.java
+++ BulletHell/src/application/Bullet.java
@@ -1,4 +1,5 @@
 package application;
+//abstract method????
 public abstract class Bullet 
 {//aod is angle of direction and it will always be >=0 and <360
  //(implement mod 360 if you need to)
@@ -8,7 +9,7 @@ public abstract class Bullet
 	String imgName;
 	int[] hBox; //bHoles do not have one, Moon bullets will be circular so array will
 				//only have radius and center.Everything else will be image coords {x1,y1,x2,y2}
-	Bullet(int x, int y, double speed, double damage, double angle, boolean isBHole, String imageName,int[] hitBox)
+	public Bullet(int x, int y, double speed, double damage, double angle, boolean isBHole, String imageName,int[] hitBox)
 	{
 		bSpeed=speed;bDamage=damage;aod=angle; bHole=isBHole; imgName=imageName; 
 		hBox=hitBox;
